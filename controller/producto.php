ea028d2d8ced9db89d599dfd7b0acb19f4ce229b
Isa-josep <isaurini1902@gmail.com>
2024-05-20 00:09:22 -0600

    termiando

--- controller/producto.php
+++ controller/producto.php
@@ -4,35 +4,32 @@
     $producto = new Producto();
     switch($_GET["op"]){
         case "insert":
-            $datos=$producto->insert_producto($_POST["rule_name"]);
-            if(empty($_FILES["file"]["name"])){
+            $datos = $producto->insert_producto($_POST["rule_name"]);
+            if (empty($_FILES["file"]["name"][0])) {
                 // No se han subido archivos
-            }
-            else{
-                $countfiles = count($_FILES["file"]["name"]);
-                $ruta ="../assets/";
-                $files_arr = array();
-                if(!file_exists($ruta)){
-                    mkdir($ruta, 0777, true);
-                }
-                for($index = 0; $index < $countfiles; $index++){
-                    $filename = $_FILES["file"]["name"][$index];
-                    $tmp_filename = $_FILES["file"]["tmp_name"][$index];
-                    $destino = $ruta . $filename;
-                    foreach($datos as $row){
-                        $producto->insert_file($row["rule_id"], $destino); // destino si se quiere obtener el path
+            } else {
+                foreach ($datos as $row) {
+                    $countfiles = count($_FILES["file"]["name"]);
+                    $ruta = "../assets/" . $row["rule_id"] . "/";
+                    $files_arr = array();
+                    if (!file_exists($ruta)) {
+                        mkdir($ruta, 0777, true);
                     }
-                    
-
-                    if(move_uploaded_file($tmp_filename, $destino)){
-                        $files_arr[] = $destino; 
-                    } else {
-                        echo "Error al mover el archivo: $filename";
+                    for ($index = 0; $index < $countfiles; $index++) {
+                        $filename = $_FILES["file"]["name"][$index];
+                        $tmp_filename = $_FILES["file"]["tmp_name"][$index];
+                        $destino = $ruta . $filename;
+                        
+                        if (move_uploaded_file($tmp_filename, $destino)) {
+                            $producto->insert_file($row["rule_id"], $destino); // Guardar la ruta del archivo en la base de datos
+                            $files_arr[] = $destino; 
+                        } else {
+                            echo "Error al mover el archivo: $filename";
+                        }
                     }
                 }
             }
             echo json_encode($datos);
         break;
     }
-
 ?>
